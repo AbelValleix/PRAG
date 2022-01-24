08940ea6fb76d092b32657116a49d2820aabce45
AbelValleix <44614113+AbelValleix@users.noreply.github.com>
2022-01-24 15:04:30 +0100

    Update WindSpeedType.java

--- Weather/src/InformationTemperature/WindSpeedType.java
+++ Weather/src/InformationTemperature/WindSpeedType.java
@@ -14,6 +14,7 @@ public class WindSpeedType {
 
     public void setWindSpeed(double windSpeed) {
         if(windSpeed >0 || windSpeed <450 )
+            //Une vitesse de vent ne peut être négative, et si elle est raisonnable, ne peut dépasser la valeur 450
             this.windSpeed = windSpeed;
         else
             throw new IllegalArgumentException("Vitesse incorrect");
