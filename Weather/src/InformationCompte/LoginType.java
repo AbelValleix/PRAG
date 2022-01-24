e1a5dd34665c268b958526e080a5d7e673e292c7
AbelValleix <44614113+AbelValleix@users.noreply.github.com>
2022-01-24 15:06:08 +0100

    Update LoginType.java

--- Weather/src/InformationCompte/LoginType.java
+++ Weather/src/InformationCompte/LoginType.java
@@ -15,6 +15,7 @@ public class LoginType {
     public void setLogin(String login) {
         boolean b = Pattern.matches("[a-zA-Z0-9]", login);
         if (b = true && login.length() < 16) {
+            //Le nom d'utilisateur ne peut être construit qu'avec des lettres ou des numéros, et doit faire moins de 16 caractères. 
             this.login = login;
         } else {
             throw new IllegalArgumentException("Login incorrect");
