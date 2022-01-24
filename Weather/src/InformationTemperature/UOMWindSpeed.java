55b6692305c9d75038303b3faacb6b3ca0df01f4
AbelValleix <44614113+AbelValleix@users.noreply.github.com>
2022-01-24 15:03:48 +0100

    Update UOMWindSpeed.java

--- Weather/src/InformationTemperature/UOMWindSpeed.java
+++ Weather/src/InformationTemperature/UOMWindSpeed.java
@@ -2,7 +2,7 @@ package InformationTemperature;
 public enum UOMWindSpeed {
     km("km/h"),
     mph("m/h");
-
+    //L'unité de vitesse de vent est définie en km/h ou miles/h
     UOMWindSpeed(String valeur){
         this.valeur = valeur;
     }
